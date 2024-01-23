bdc7295c120de3dceabaf110becfb50087c30d79
marla294@gmail.com <marla294@gmail.com>
2024-01-23 16:49:34 -0600

    Setting up IngredientsList component

--- pages/ingredients.tsx
+++ pages/ingredients.tsx
@@ -2,17 +2,58 @@ import dbConnect from "../lib/dbconnect";
 import Ingredient from "../models/Ingredient";
 import IngredientImage from "../models/IngredientImage";
 import User from "../models/User";
+import styled from "styled-components";
+
+const ListItemStyles = styled.div`
+  background: white;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: 10rem 5fr;
+
+  img {
+    object-fit: cover;
+    height: 10rem;
+    min-width: 10rem;
+    width: 5rem;
+  }
+
+  .noPhoto {
+    height: 10rem;
+    min-width: 10rem;
+    width: 5rem;
+  }
+
+  .details {
+    width: 100%;
+    padding: 0.5rem;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-left: 2rem;
+    align-self: center;
+    justify-self: center;
+  }
+`;
 
 export default function Ingredients({ ingredients }) {
   console.log({ ingredients });
   return (
     <div>
       {ingredients?.map((ingredient) => (
-        <div key={ingredient?._id}>
-          <div>{ingredient?.name}</div>
-          <div>{ingredient?.user?.name}</div>
-          <img src={ingredient?.photo?.image?._meta?.url} />
-        </div>
+        <ListItemStyles key={ingredient?._id}>
+          {ingredient?.photo?.image?._meta?.url ? (
+            <img
+              src={ingredient?.photo?.image?._meta?.url}
+              alt={ingredient?.photo?.altText || ingredient?.name}
+            />
+          ) : (
+            <div className="noPhoto">No Photo</div>
+          )}
+
+          <div className="details">{ingredient?.name}</div>
+        </ListItemStyles>
       ))}
     </div>
   );
