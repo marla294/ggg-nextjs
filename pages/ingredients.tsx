b1c03ed6c536675a81268563f93b605ea048db20
marla294@gmail.com <marla294@gmail.com>
2024-01-23 16:09:53 -0600

    Adding photo link to Ingredient

--- pages/ingredients.tsx
+++ pages/ingredients.tsx
@@ -1,23 +1,10 @@
 import dbConnect from "../lib/dbconnect";
-import clientPromise from "../lib/mongodb";
 import Ingredient from "../models/Ingredient";
 import IngredientImage from "../models/IngredientImage";
 import User from "../models/User";
 
-// interface Ingredient {
-//   _id: string;
-//   name: string;
-//   description: string;
-//   store: string;
-//   units: string;
-//   aisle: string;
-//   homeArea: string;
-//   user: string;
-//   photo: Object;
-// }
-
 export default function Ingredients({ ingredients }) {
-  // console.log({ ingredients });
+  console.log({ ingredients });
   return (
     <div>
       {ingredients?.map((ingredient) => (
@@ -33,13 +20,18 @@ export default function Ingredients({ ingredients }) {
 export async function getServerSideProps() {
   try {
     await dbConnect();
-    // const db = client.db("ggg");
 
     const ingredients = await Ingredient.find({})
       .populate([
         {
           path: "user",
           model: User,
+        },
+      ])
+      .populate([
+        {
+          path: "photo",
+          model: IngredientImage,
         },
       ])
       .exec();
