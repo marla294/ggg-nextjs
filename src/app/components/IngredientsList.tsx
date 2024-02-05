2917f207b46295ee2c45b1e7a7bdc827d87d61b2
marla294@gmail.com <marla294@gmail.com>
2024-02-05 06:53:49 -0600

    Styling search ingredients

--- src/app/components/IngredientsList.tsx
+++ src/app/components/IngredientsList.tsx
@@ -1,6 +1,24 @@
 "use client";
 import { useState } from "react";
 import IngredientListItem from "./IngredientListItem";
+import styled from "styled-components";
+
+const IngredientsBarStyles = styled.div`
+  display: grid;
+  grid-gap: 0.5rem;
+  font-size: 0.8rem;
+
+  input,
+  select {
+    padding: 0.3rem;
+    border: 1px solid black;
+    font-size: 1.4rem;
+    height: 4rem;
+    &:focus {
+      outline: 0;
+    }
+  }
+`;
 
 const IngredientsList = ({
   ingredients,
@@ -13,13 +31,15 @@ const IngredientsList = ({
 
   return (
     <div>
-      <input
-        name="searchTerm"
-        id="searchTerm"
-        placeholder="Search..."
-        // value={inputs.searchTerm}
-        // onChange={handleChange}
-      />
+      <IngredientsBarStyles>
+        <input
+          name="searchTerm"
+          id="searchTerm"
+          placeholder="Search..."
+          // value={inputs.searchTerm}
+          // onChange={handleChange}
+        />
+      </IngredientsBarStyles>
       {displayIngredients?.map((ingredient: any) => (
         <IngredientListItem key={ingredient._id} ingredient={ingredient} />
       ))}
--- src/app/ingredients/page.tsx
+++ src/app/ingredients/page.tsx
@@ -4,12 +4,12 @@ import IngredientsList from "../components/IngredientsList";
 
 export default async function Ingredients() {
   const res = await getIngredients({ name: null });
-  const ingredients = JSON.parse(res as string);
+  const initialIngredients = JSON.parse(res as string);
 
   return (
     <>
       <IngredientsList
-        ingredients={ingredients}
+        ingredients={initialIngredients}
         getIngredients={getIngredients}
       />
     </>
