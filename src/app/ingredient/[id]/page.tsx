9c651bf1b1e7fe2326d94efca370850ba593550b
marla294@gmail.com <marla294@gmail.com>
2024-02-09 06:34:57 -0600

    Styling single ingredient page

--- src/app/ingredient/[id]/page.tsx
+++ src/app/ingredient/[id]/page.tsx
@@ -1,6 +1,35 @@
 "use client";
 import { useEffect, useState } from "react";
 import getIngredients from "../../ingredients/getIngredients";
+import styled from "styled-components";
+
+const SingleItemStyles = styled.div`
+  padding: 0 10%;
+
+  img {
+    width: 100%;
+    max-height: 300px;
+    object-fit: contain;
+  }
+  .noPhoto {
+    width: 100%;
+    max-height: 300px;
+    display: grid;
+    align-content: center;
+    justify-content: center;
+    border: 1px dashed var(--black);
+  }
+
+  @media (min-width: 768px) {
+    padding: 0;
+    display: grid;
+    grid-auto-columns: 1fr;
+    grid-auto-flow: column;
+    max-width: var(--maxWidth);
+    align-items: top;
+    grid-gap: 5rem;
+  }
+`;
 
 export default function Page({ params }: { params: { id: string } }) {
   const [ingredient, setIngredient] = useState<any>();
@@ -16,18 +45,24 @@ export default function Page({ params }: { params: { id: string } }) {
   }, []);
 
   return (
-    <div>
-      {ingredient?.photo?.image?._meta?.url && (
-        <img
-          src={ingredient?.photo?.image?._meta?.url}
-          alt={ingredient?.photo?.altText || ingredient?.name}
-        />
-      )}
-      <h3>{ingredient?.name}</h3>
-      <div>Aisle: {ingredient?.aisle}</div>
-      <div>Home Area: {ingredient?.homeArea}</div>
-      <div>Units: {ingredient?.units}</div>
-      <div>Store: {ingredient?.store}</div>
-    </div>
+    <SingleItemStyles>
+      <div>
+        {ingredient?.photo?.image?._meta?.url ? (
+          <img
+            src={ingredient?.photo?.image?._meta?.url}
+            alt={ingredient?.photo?.altText || ingredient?.name}
+          />
+        ) : (
+          <div className="noPhoto">Needs photo 📸</div>
+        )}
+      </div>
+      <div>
+        <h3>{ingredient?.name}</h3>
+        <div>Aisle: {ingredient?.aisle}</div>
+        <div>Home Area: {ingredient?.homeArea}</div>
+        <div>Units: {ingredient?.units}</div>
+        <div>Store: {ingredient?.store}</div>
+      </div>
+    </SingleItemStyles>
   );
 }
