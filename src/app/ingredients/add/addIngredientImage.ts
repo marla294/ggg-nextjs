4f7d89d58ea94ecbf42ae8ab432fa9166dd2cfee
marla294@gmail.com <marla294@gmail.com>
2024-02-12 15:50:10 -0600

    WIP - upload ingredient images

--- src/app/ingredients/add/addIngredientImage.ts
+++ src/app/ingredients/add/addIngredientImage.ts
@@ -1,22 +1,28 @@
 "use server";
+import fs from 'fs';
 import { ObjectId } from "mongodb";
 import dbConnect from "../../../../lib/dbconnect";
 import IngredientImage from "../../../../models/IngredientImage";
 import User from "../../../../models/User";
 import { getSession } from "../../../../services/authentication/cookie-session";
+import cloudinary from 'cloudinary';
 
 export default async ({
   altText,
-  url
+  filePath
 }: {
   altText?: string | null | undefined, 
-  url?: string | null | undefined, 
+  filePath?: string | null | undefined, 
   }) => {
   try {
     await dbConnect();
 
     const session = await getSession();
     const [user] = await User.find({email: session?.login});
+
+    if (filePath) {
+      const stream = fs.createReadStream(filePath);
+    }
 
     // const ingredient = Ingredient.create([{name, user: new ObjectId(user._id), store, units, aisle, homeArea, description}]);
 
