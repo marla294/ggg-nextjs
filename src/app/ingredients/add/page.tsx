ed988fc756a2711a05ce4a0da90f546c75dde349
marla294@gmail.com <marla294@gmail.com>
2024-03-01 07:08:33 -0600

    Cleanup

--- src/app/ingredients/add/page.tsx
+++ src/app/ingredients/add/page.tsx
@@ -1,91 +1,10 @@
 "use client";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { useState } from "react";
 import addIngredient from "./addIngredient";
 import addIngredientImage from "./addIngredientImage";
-import styled from "styled-components";
-import stores from "../../lib/stores";
-import units from "../../lib/units";
-import aisles from "../../lib/aisles";
-import homeAreas from "../../lib/homeAreas";
 import { useRouter } from "next/navigation";
-import { ThreeDots } from "react-loader-spinner";
 import useForm from "../../lib/useForm";
 import IngredientForm from "../../components/IngredientForm";
-
-const FormStyles = styled.form`
-  box-shadow: var(--bs);
-  padding: 2rem;
-  font-size: 1.2rem;
-  label {
-    display: block;
-    font-weight: 450;
-    margin-bottom: 0.25rem;
-  }
-  input,
-  textarea,
-  select {
-    width: 100%;
-    padding: 0.5rem;
-    margin-bottom: 0.25rem;
-    font-size: 1.2rem;
-    border: 1px solid black;
-    &:focus {
-      outline: 0;
-      border-color: var(--orange);
-    }
-  }
-  h2 {
-    font-weight: 350;
-  }
-  button {
-    width: auto;
-    padding: 0.7rem 1rem;
-    transition: 0.2s;
-  }
-
-  .submit {
-    background: var(--lime);
-    color: var(--darkGreen);
-    border: 1px solid var(--darkGreen);
-    &:hover {
-      background: var(--darkGreen);
-      color: white;
-    }
-  }
-
-  .clear {
-    background: var(--yellow);
-    color: var(--darkYellow);
-    border: 1px solid var(--darkYellow);
-    &:hover {
-      background: var(--darkYellow);
-      color: white;
-    }
-  }
-
-  .cancel {
-    background: var(--orange);
-    color: var(--darkOrange);
-    border: 1px solid var(--darkOrange);
-    &:hover {
-      background: var(--darkOrange);
-      color: white;
-    }
-  }
-`;
-
-const LoadingContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  width: 500px;
-  align-items: center;
-  margin-top: 1rem;
-  grid-gap: 2rem;
-`;
-
-const LoadingStyles = styled.div`
-  color: green;
-`;
 
 export default function AddIngredient() {
   const router = useRouter();
