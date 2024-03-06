6d3f54e15f70b68957769f762fa1eb0e31499921
marla294@gmail.com <marla294@gmail.com>
2024-03-06 08:02:00 -0600

    Creating shoppingListItemSchema

--- models/ShoppingListItem.ts
+++ models/ShoppingListItem.ts
@@ -4,6 +4,14 @@ const shoppingListItemSchema = new Schema({
   quantity: {
     type: Number,
   },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  ingredient: {
+    type: Schema.Types.ObjectId,
+    ref: 'Ingredient'
+  }
 });
 
 const ShoppingListItem = models.ShoppingListItem || model("ShoppingListItem", shoppingListItemSchema);
