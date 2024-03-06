532a3770efe05c277c4bc9b449c7cf28135397c1
marla294@gmail.com <marla294@gmail.com>
2024-03-06 08:06:57 -0600

    WIP - ShoppingList page

--- src/app/shoppingList/page.tsx
+++ src/app/shoppingList/page.tsx
@@ -1,8 +1,11 @@
 "use client";
-
+import { useState } from "react";
 import { BarContainer, ListContainer } from "../ingredients/page";
 
 export default function ShoppingList() {
+  const [displayShoppingListItems, setDisplayShoppingListItems] =
+    useState<any>(null);
+
   return (
     <div>
       <BarContainer>
