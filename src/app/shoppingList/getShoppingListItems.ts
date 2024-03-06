71b5004e84a22ca8c9cab14872a8c90f104b6e38
marla294@gmail.com <marla294@gmail.com>
2024-03-06 08:05:05 -0600

    Creating getShoppingListItem

---
+++ src/app/shoppingList/getShoppingListItems.ts
@@ -0,0 +1,40 @@
+"use server";
+import dbConnect from "../../../lib/dbconnect";
+import Ingredient from "../../../models/Ingredient";
+import ShoppingListItem from "../../../models/ShoppingListItem";
+import User from "../../../models/User";
+import { getSession } from "../../../services/authentication/cookie-session";
+
+export default async () => {
+  try {
+    await dbConnect();
+
+    const session = await getSession();
+
+    const shoppingListItems = await ShoppingListItem.find({})
+      .populate([
+        {
+          path: "user",
+          model: User,
+        },
+      ])
+      .populate([
+        {
+          path: "ingredient",
+          model: Ingredient,
+        },
+      ])
+      .exec();
+
+    if (shoppingListItems) {
+      const shoppingListItemsFiltered = shoppingListItems
+        .filter((shoppingListItem) => shoppingListItem.user?.email === session?.login)
+        .sort((a, b) => (a.name < b.name ? -1 : 1));
+      return JSON.stringify(shoppingListItemsFiltered);
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
