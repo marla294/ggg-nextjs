f1e74d64bd3d295c6fbde42c68f242e0392c0b0b
marla294@gmail.com <marla294@gmail.com>
2024-04-17 08:19:29 -0500

    WIP - recipes list page

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,5 +1,30 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import getRecipes from "./getRecipes";
+import { ListContainer } from "../ingredients/page";
+import IngredientListItem from "../components/IngredientListItem";
+
 export default function Recipes() {
-  return <div>Recipes Page</div>;
+  const [displayRecipes, setDisplayRecipes] = useState<any>(null);
+
+  const fetchRecipes = async () => {
+    const res = await getRecipes();
+    const tempRecipes = JSON.parse(res as string);
+    setDisplayRecipes(tempRecipes);
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  return (
+    <div>
+      <ListContainer>
+        {displayRecipes?.map((ingredient: any) => (
+          <IngredientListItem key={ingredient._id} ingredient={ingredient} />
+        ))}
+      </ListContainer>
+    </div>
+  );
 }
