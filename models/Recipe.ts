39f74ba200fd460be42a2f7b9970b7c5d417f148
marla294@gmail.com <marla294@gmail.com>
2024-04-17 08:08:13 -0500

    Adding Recipe model

---
+++ models/Recipe.ts
@@ -0,0 +1,25 @@
+import { Schema, model, models } from 'mongoose';
+
+const recipeSchema = new Schema({
+  name: {
+    type: String
+  },
+  description: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  photo: {
+    type: Schema.Types.ObjectId,
+    ref: 'IngredientImage'
+  }
+});
+
+const Recipe = models.Recipe || model("Recipe", recipeSchema);
+
+export default Recipe;
