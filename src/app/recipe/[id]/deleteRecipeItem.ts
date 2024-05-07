21751a7dcd3a78bbca2d6ca34bcf817a21446786
marla294@gmail.com <marla294@gmail.com>
2024-05-07 08:13:49 -0500

    Adding deleteRecipeItem server component

---
+++ src/app/recipe/[id]/deleteRecipeItem.ts
@@ -0,0 +1,15 @@
+"use server"
+import dbConnect from "../../../../lib/dbconnect"
+import RecipeItem from "../../../../models/RecipeItem";
+
+export default async ({recipeItemId}: {recipeItemId: any}) => {
+  try {
+    await dbConnect();
+
+    const res = RecipeItem.findByIdAndDelete(recipeItemId);
+
+    return JSON.stringify(res);
+  } catch(e) {
+    console.error(e);
+  }
+}
