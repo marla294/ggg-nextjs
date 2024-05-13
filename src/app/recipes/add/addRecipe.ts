0dbb92b09ad6ea0135d6e9771266d264b4fb8a0b
marla294@gmail.com <marla294@gmail.com>
2024-05-13 07:28:39 -0500

    Add Recipe server function

---
+++ src/app/recipes/add/addRecipe.ts
@@ -0,0 +1,38 @@
+"use server";
+import { ObjectId } from "mongodb";
+import dbConnect from "../../../../lib/dbconnect";
+// import Ingredient from "../../../../models/Ingredient";
+import User from "../../../../models/User";
+import { getSession } from "../../../../services/authentication/cookie-session";
+
+export default async ({
+  name, 
+  store,
+  units,
+  aisle,
+  homeArea,
+  description,
+  photoId,
+}: {
+    name?: string | null | undefined, 
+    store?: string | null | undefined, 
+    units?: string | null | undefined,
+    aisle?: string | null | undefined,
+    homeArea?: string | null | undefined,
+    description?: string | null | undefined,
+    photoId?: any,
+  }) => {
+  try {
+    await dbConnect();
+
+    const session = await getSession();
+    const [user] = await User.find({email: session?.login});
+
+    // const ingredient = await Ingredient.create([{name, user: new ObjectId(user._id), store, units, aisle, homeArea, description, photo: new ObjectId(photoId)}]);
+
+    // return JSON.stringify(ingredient);
+
+  } catch (e) {
+    console.error(e);
+  }
+};
