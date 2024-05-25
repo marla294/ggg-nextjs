404ddf001daca8d6ea1c660e6836a4bf7ddd10f6
marla294@gmail.com <marla294@gmail.com>
2024-05-25 06:44:14 -0500

    Add editRecipe server component

---
+++ src/app/recipe/[id]/editRecipe.ts
@@ -0,0 +1,38 @@
+"use server";
+import { ObjectId } from "mongodb";
+import dbConnect from "../../../../lib/dbconnect";
+import Recipe from "../../../../models/Recipe";
+
+export default async ({
+  id,
+  name,
+  type,
+  recipeLink,
+  description,
+  photoId,
+}: {
+    id: string,
+    name?: string | null | undefined,
+    type?: string | null | undefined,
+    recipeLink?: string | null | undefined,
+    description?: string | null | undefined,
+    photoId?: any,
+  }) => {
+  try {
+    await dbConnect();
+
+    const filter = { _id: id};
+    let update = {name, type, recipeLink, description};
+
+    if (photoId) {
+      update = {...update, photo: new ObjectId(photoId)}
+    }
+
+    const recipe = await Recipe.findOneAndUpdate(filter, update);
+
+    return JSON.stringify(recipe);
+
+  } catch (e) {
+    console.error(e);
+  }
+};
