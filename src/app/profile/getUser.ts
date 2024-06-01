0f62b966460f2cc36677c703ebf47251c3e766d3
marla294@gmail.com <marla294@gmail.com>
2024-06-01 07:14:13 -0500

    Writing getUser server function

--- src/app/profile/getUser.ts
+++ src/app/profile/getUser.ts
@@ -1,5 +1,6 @@
 "use server"
 import dbConnect from "../../../lib/dbconnect";
+import User from "../../../models/User";
 import { getSession } from "../../../services/authentication/cookie-session";
 
 export default async () => {
@@ -7,7 +8,17 @@ export default async () => {
     await dbConnect();
 
     const session = await getSession();
-    console.log({session});
+
+    const users = await User.find({}).exec();
+
+    if (users) {
+      const theUser = users
+      .filter(user => user.email === session?.login)[0];
+
+      return JSON.stringify(theUser);
+    } else {
+      return null;
+    }
   } catch (e) {
     console.error(e);
   }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+import { useEffect, useState } from "react";
+import getUser from "./getUser";
+
 export default function Page() {
+  const [user, setUser] = useState();
+
+  const fetchUser = async () => {
+    const res = await getUser();
+    // const tempRecipeItems = JSON.parse(res);
+    // setRecipeItems(tempRecipeItems);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return <>Profile</>;
 }
