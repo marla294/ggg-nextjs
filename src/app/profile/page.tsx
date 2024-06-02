4c72662e6a5f80487213914fecf1e49c7bf6fdb6
marla294@gmail.com <marla294@gmail.com>
2024-06-02 06:36:55 -0500

    WIP - Profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,19 +1,38 @@
 "use client";
 import { useEffect, useState } from "react";
 import getUser from "./getUser";
+import styled from "styled-components";
+
+const Line = styled.div`
+  display: grid;
+  width: 400px;
+  grid-template-columns: 1fr 5fr;
+`;
 
 export default function Page() {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState<any>();
 
   const fetchUser = async () => {
     const res = await getUser();
-    // const tempRecipeItems = JSON.parse(res);
-    // setRecipeItems(tempRecipeItems);
+    const tempUser = JSON.parse(res || "");
+    setUser(tempUser);
   };
 
   useEffect(() => {
     fetchUser();
   }, []);
 
-  return <>Profile</>;
+  return (
+    <div>
+      <h2>Your Profile</h2>
+      <Line>
+        <h4>Name: </h4>
+        <span>{user?.name}</span>
+      </Line>
+      <Line>
+        <h4>Email: </h4>
+        <span>{user?.email}</span>
+      </Line>
+    </div>
+  );
 }
