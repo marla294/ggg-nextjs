355579f2df9347de87ea6abfc1cf92117bdcb4b2
marla294@gmail.com <marla294@gmail.com>
2024-06-03 06:36:49 -0500

    WIP - change password

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,14 +9,97 @@ const Line = styled.div`
   grid-template-columns: 1fr 5fr;
 `;
 
+const FormStyles = styled.form`
+  box-shadow: var(--bs);
+  padding: 2rem;
+  font-size: 1.2rem;
+  label {
+    display: block;
+    font-weight: 450;
+    margin-bottom: 0.25rem;
+  }
+  input,
+  textarea,
+  select {
+    width: 100%;
+    padding: 0.5rem;
+    margin-bottom: 0.25rem;
+    font-size: 1.2rem;
+    border: 1px solid black;
+    &:focus {
+      outline: 0;
+      border-color: var(--orange);
+    }
+  }
+  h2 {
+    font-weight: 350;
+  }
+  button {
+    width: auto;
+    padding: 0.7rem 1rem;
+    margin: 1.5rem 1rem 0.5rem 0;
+    transition: 0.2s;
+  }
+
+  .submit {
+    background: var(--lime);
+    color: var(--darkGreen);
+    border: 1px solid var(--darkGreen);
+    &:hover {
+      background: var(--darkGreen);
+      color: white;
+    }
+  }
+
+  .clear {
+    background: var(--yellow);
+    color: var(--darkYellow);
+    border: 1px solid var(--darkYellow);
+    &:hover {
+      background: var(--darkYellow);
+      color: white;
+    }
+  }
+
+  .cancel {
+    background: var(--orange);
+    color: var(--darkOrange);
+    border: 1px solid var(--darkOrange);
+    &:hover {
+      background: var(--darkOrange);
+      color: white;
+    }
+  }
+`;
+
+const Error = styled.div`
+  color: red;
+  border: 1px solid red;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
 export default function Page() {
   const [user, setUser] = useState<any>();
+  const [inputs, setInputs] = useState({ password: "" });
+  const [error, setError] = useState<string | null>(null);
 
   const fetchUser = async () => {
     const res = await getUser();
     const tempUser = JSON.parse(res || "");
     setUser(tempUser);
   };
+
+  const handleChange = (e: any) => {
+    const { value, name } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const handleChangePassword = async () => {};
 
   useEffect(() => {
     fetchUser();
@@ -33,6 +116,32 @@ export default function Page() {
         <h4>Email: </h4>
         <span>{user?.email}</span>
       </Line>
+      <FormStyles
+        method="POST"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          // await handleChangePassword();
+        }}>
+        <fieldset>
+          <h2>Change your password</h2>
+          {error && <Error>{error}</Error>}
+          <label htmlFor="password">
+            Password<span className="required">&nbsp;*</span>
+            <input
+              required
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={inputs.password}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit" className="submit">
+            Change Password
+          </button>
+        </fieldset>
+      </FormStyles>
     </div>
   );
 }
