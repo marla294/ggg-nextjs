f1d27f00c6912ef13e515f57f0fef8dd0b098f0e
marla294@gmail.com <marla294@gmail.com>
2024-06-04 06:34:43 -0500

    WIP - change password

---
+++ src/app/profile/changePassword.ts
@@ -0,0 +1,37 @@
+"use server"
+import dbConnect from "../../../lib/dbconnect";
+import User from "../../../models/User";
+import bcryptjs from 'bcryptjs';
+import { setSession } from "../../../services/authentication/cookie-session";
+
+export default async ({email, password}: {email: string, password: string}) => {
+  try {
+    await dbConnect();
+
+    const users = await User.find({email}).exec();
+    const user = users[0];
+
+    if (user) {
+      const encryptedPassword = await bcryptjs.encodeBase64(password, 60);
+
+      // const authResult = await bcryptjs.compare(password, user.password);
+      // if (authResult) {
+      //   await setSession({login: email});
+      //   return {
+      //     success: true
+      //   }
+      // }
+      // else {
+      //   return {
+      //     success: false,
+      //     error: 'Invalid username or password'
+      //   }
+      // }
+    }
+
+    return;
+
+  } catch (e) {
+    console.error(e);
+  }
+}
