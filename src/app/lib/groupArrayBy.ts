f7d3ad8de88b08ca25b29a0e53630f10f6811e09
marla294@gmail.com <marla294@gmail.com>
2024-07-18 06:13:26 -0500

    Fixing issue with sorting on shopping list page

--- src/app/lib/groupArrayBy.ts
+++ src/app/lib/groupArrayBy.ts
@@ -8,8 +8,6 @@ const capitalize = (s: string) => {
 
 export default function groupArrayBy(arr: any[] = [], groupBy: any, subProperty?: string) {
 
-  console.log({arr, groupBy});
-
   const grouping = arr.reduce((groupingObject: any, currentVal: any) => {
     let current = currentVal;
     if (subProperty) {
@@ -17,7 +15,8 @@ export default function groupArrayBy(arr: any[] = [], groupBy: any, subProperty?
     }
 
     // Get value of current object property to group on
-    const currentGroup = current[groupBy];
+    let currentGroup = current[groupBy];
+    
     if (!currentGroup) {
       groupingObject['uncategorized'] = [
         ...(groupingObject['uncategorized'] || []),
@@ -25,6 +24,11 @@ export default function groupArrayBy(arr: any[] = [], groupBy: any, subProperty?
       ];
       return groupingObject;
     };
+
+    if (typeof currentGroup !== 'string') {
+      currentGroup = currentGroup['name'];
+    }
+
     const currentGroupLowercase = capitalize((currentGroup as string).toLowerCase());
     // If the groupingArray already has a grouping for this, add current value to it, if not then create a new one
     groupingObject[currentGroupLowercase] = [
