36a38a9d6faeea750dafe63a19a8dbe402e48dca
marla294@gmail.com <marla294@gmail.com>
2024-07-24 06:18:34 -0500

    RecipeTypes page

---
+++ src/app/recipeTypes/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import getRecipeTypes from "./getRecipeTypes";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Link from "next/link";
+// import StoreListItem from "../components/StoreListItem";
+
+const ListContainer = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  margin-top: 4rem;
+`;
+
+const HeaderContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  width: 280px;
+  align-items: center;
+`;
+
+const AddRecipeTypeLinkText = styled.div`
+  font-size: 1rem;
+  color: var(--green);
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export default function RecipeTypes() {
+  const [recipeTypes, setRecipeTypes] = useState<any>(null);
+
+  const fetchRecipeTypes = async () => {
+    const res = await getRecipeTypes();
+    const tempRecipeTypes = JSON.parse(res as string);
+    setRecipeTypes(tempRecipeTypes);
+  };
+
+  useEffect(() => {
+    fetchRecipeTypes();
+  }, []);
+
+  return (
+    <div>
+      <HeaderContainer>
+        <h3>Recipe Types</h3>
+        <Link href="stores/add">
+          <AddRecipeTypeLinkText>+ Add RecipeType</AddRecipeTypeLinkText>
+        </Link>
+      </HeaderContainer>
+
+      <ListContainer>
+        {recipeTypes?.map((recipeType: any) => (
+          <div key={recipeType?._id}>{recipeType?.name}</div>
+          // <StoreListItem store={store} fetchStores={fetchRecipeTypes} />
+        ))}
+      </ListContainer>
+    </div>
+  );
+}
