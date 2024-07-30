d202c0539d85a0f77f0f67f8f466eb9a46c7df2a
marla294@gmail.com <marla294@gmail.com>
2024-07-30 09:12:17 -0500

    HomeAreas page

---
+++ src/app/homeAreas/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import getHomeAreas from "./getHomeAreas";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Link from "next/link";
+// import StoreListItem from "../components/StoreListItem";
+
+const ListContainer = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  margin-top: 4rem;
+`;
+
+const HeaderContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  width: 280px;
+  align-items: center;
+`;
+
+const AddStoreLinkText = styled.div`
+  font-size: 1rem;
+  color: var(--green);
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export default function HomeArea() {
+  const [homeAreas, setHomeAreas] = useState<any>(null);
+
+  const fetchHomeAreas = async () => {
+    const res = await getHomeAreas();
+    const tempHomeAreas = JSON.parse(res as string);
+    setHomeAreas(tempHomeAreas);
+  };
+
+  useEffect(() => {
+    fetchHomeAreas();
+  }, []);
+
+  return (
+    <div>
+      <HeaderContainer>
+        <h3>Home Areas</h3>
+        <Link href="homeAreas/add">
+          <AddStoreLinkText>+ Add Home Area</AddStoreLinkText>
+        </Link>
+      </HeaderContainer>
+
+      <ListContainer>
+        {homeAreas?.map((homeArea: any) => (
+          <div id={homeArea?._id} key={homeArea?._id}>
+            {homeArea?.name}
+          </div>
+        ))}
+        {/* {homeAreas?.map((homeArea: any) => (
+          <StoreListItem store={homeArea} fetchStores={fetchHomeAreas} />
+        ))} */}
+      </ListContainer>
+    </div>
+  );
+}
