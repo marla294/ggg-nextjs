6cbe1a2b3f82ca085c112d8cdbf2d0fdb4385426
marla294@gmail.com <marla294@gmail.com>
2024-08-14 06:20:14 -0500

    Working on shared button styles component

---
+++ src/app/components/styles/ButtonStyles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ButtonStyles = styled.button`
+  width: 300px;
+  height: 40px;
+  transition: 0.2s;
+  margin: 0 !important;
+  padding: 0.7rem 1rem;
+`;
+
+export default ButtonStyles;
